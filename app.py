bd2ab96a8545f8493bfc5c1770f1d3381e2ba86d
Michael Roberts <marobs@umich.edu>
2017-06-26 10:29:40 -0400

    No longer reloads on code change

--- app.py
+++ app.py
@@ -14,4 +14,4 @@ app.secret_key = helpers.getFlaskSecret()
 # For us, listen to port 3000 so you can just run 'python app.py' to start the server
 if __name__ == '__main__':
     # listen on external IPs
-    app.run(host='0.0.0.0', port=3000, debug=True)
+    app.run(host='0.0.0.0', port=3000, debug=True, use_reloader=False)
