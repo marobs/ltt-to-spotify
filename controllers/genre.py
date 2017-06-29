968e654ff7e3f0be38e8728a7f64e6405857fafa
Michael Roberts <marobs@umich.edu>
2017-06-29 13:17:15 -0400

    Changed meta tag description

--- controllers/genre.py
+++
@@ -1,9 +0,0 @@
-from flask import *
-import helpers
-import os
-
-genre = Blueprint('genre', __name__, template_folder='templates')
-
-@genre.route('/genre')
-def genre_route():
-	return render_template("404.html"), 404
