e94f32179cc22a7d1077d15b0b7614b83dab0451
Michael Roberts <marobs@umich.edu>
2017-07-12 15:21:42 -0400

    Debugging cache

--- ltt/ltt_reddit.py
+++ ltt/ltt_reddit.py
@@ -9,6 +9,7 @@ def getRedditPosts(redditQuery):
     # Check if the data is cached
     cachedData = helpers.getFromRCache(redditQuery)
     if cachedData is not None:
+        print "Found and returning cached reddit data"
         return cachedData
 
     # Grab actual reddit data
@@ -25,15 +26,14 @@ def getRedditPosts(redditQuery):
             continue
 
         post = {}
-        if 'title' in child['data'] and 'url' in child['data']:
-            post['rawTitle'] = child['data']['title']
-            post['url'] = child['data']['url']
-            post['redditId'] = child['data']['id']
-
-        post = fillPostData(post)
-        postList.append(post)
+        if 'title' in child['data'] and 'url' in child['data'] and 'id' in child['data']:
+            post = fillPostData(post, child)
+            if post is not None:
+                postList.append(post)
 
     # Save to cache
+    print "Saving to cache: "
+    printPostList(postList)
     helpers.saveToRCache(postList, redditQuery)
     return postList
 
@@ -47,7 +47,11 @@ def queryForRedditData(redditQuery):
 
 
 # Use regex to pull out title, artist, genre, and year
-def fillPostData(post):
+def fillPostData(post, child):
+    post['rawTitle'] = child['data']['title']
+    post['url'] = child['data']['url']
+    post['redditId'] = child['data']['id']
+
     regexGroups = re.search("^(.*) -{1,2} (.*) \[(.*)\].*(\d\d\d\d)", post["rawTitle"])
     if regexGroups is None:
         regexGroups = re.search("^(.*) -{1,2} (.*) \[(.*)\]", post["rawTitle"])
